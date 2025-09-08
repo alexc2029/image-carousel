030de0f4ae3de4a87ca778b59611d945a4fcd53a
alexc2029 <114008402+alexc2029@users.noreply.github.com>
2025-09-08 15:23:53 +0300

    Implement correct element positioning and arrows to cycle slides

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import "./styles.css";
+
+const imageFrame = document.querySelector(".image-frame");
+const imageFrameWidth = getComputedStyle(imageFrame).width;
+const imagesContainer = document.querySelector(".images");
+const images = imagesContainer.getElementsByTagName("img");
+const imageCount = images.length;
+
+function nextImageSlide() {
+	let leftValue = parseInt(getComputedStyle(imagesContainer).left);
+	if (
+		parseInt(imageFrameWidth) * imageCount + leftValue >
+		parseInt(imageFrameWidth)
+	) {
+		imagesContainer.style.left =
+			leftValue - parseInt(imageFrameWidth) + "px";
+	}
+}
+
+function previousImageSlide() {
+	let leftValue = parseInt(getComputedStyle(imagesContainer).left);
+	if (leftValue < 0) {
+		imagesContainer.style.left =
+			leftValue + parseInt(imageFrameWidth) + "px";
+	}
+}
+
+const previousImageButton = document.querySelector("button.previous-image");
+const nextImageButton = document.querySelector("button.next-image");
+
+previousImageButton.addEventListener("click", previousImageSlide);
+nextImageButton.addEventListener("click", nextImageSlide);
